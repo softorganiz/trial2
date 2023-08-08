127ab17fea7fc5208f074e5fbb32a4959b550fd8
mona <mona@192.168.0.102>
2023-08-08 19:19:24 +0300

    ..

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
-package org.example;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main {
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        logger.log(Level.INFO, "Hello, World!");
     }
 }
